e762f61d6e20c21eda69b2651516582ae8ddafd1
eduars <68311868+eduarsc@users.noreply.github.com>
2024-03-03 17:51:22 -0300

    add js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,25 @@
 
+document.getElementById('downloadPdf').addEventListener('click', function () {
+    const element = document.querySelector('.container'); // Selecciona el div con clase 'container'
+
+    html2pdf()
+      .set({
+        margin: 1,
+        filename: 'resume.pdf',
+        image: {
+          type: 'jpeg',
+          quality: 0.98
+        },
+        html2canvas: {
+          scale: 3, // Ajusta la escala si es necesario para mejorar la calidad de la imagen
+          letterRendering: true,
+        },
+        jsPDF: {
+          unit: 'in',
+          format: 'letter',
+          orientation: 'portrait'
+        }
+      })
+      .from(element)
+      .save();
+});
